9d3cea727b79df2307d463db8d787117b3092b66
ShadowGreg <shadow-greg@yandex.ru>
2023-01-24 20:55:33 +0300

    Круто увеличил скорость работы алгоритма более чем в 30 раз

--- Lesson002/Lesson/Task001/src/Main.java
+++ Lesson002/Lesson/Task001/src/Main.java
@@ -1,11 +1,17 @@
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        String jewels = "aBC";
-        String stones = "aaaAbbbBCCDDFF";
+        String jewels = "aBCDGH";
+        String stones = "aaaAbbbBCsdfsAdfgsdfASDSDFASXCVBXCVBDSFGHSDFGSDFAWEASGJRYJYUYUJYJDFHDGFAWEFWEFAWFASCDDFF";
         System.out.println(findJewelsInStones(jewels, stones));
+        System.out.println(findJewelsInStonesDic(jewels, stones));
     }
 
     public static String findJewelsInStones(String jewels, String stones) {
+        long startTime = System.nanoTime();
         String outputResult = "";
         int counter = 0;
         for (int i = 0; i < jewels.length(); i++) {
@@ -18,6 +24,27 @@ public class Main {
             outputResult = outputResult + jewels.charAt(i) + counter;
             counter = 0;
         }
+        long endTime = System.nanoTime();
+        System.out.println((endTime - startTime) / 1000000 + "ms");
+        return outputResult;
+    }
+
+    public static Map<Character, Integer> findJewelsInStonesDic(String jewels, String stones) {
+        long startTime = System.nanoTime();
+        Map<Character, Integer> outputResult = new HashMap<>();
+        for (int i = 0; i < jewels.length(); i++) {
+            outputResult.put(jewels.charAt(i), 0);
+        }
+
+        for (int i = 0; i < stones.length(); i++) {
+            if (outputResult.containsKey(stones.charAt(i))) {
+                int temp = outputResult.get(stones.charAt(i));
+                outputResult.put(stones.charAt(i), ++temp);
+            }
+        }
+
+        long endTime = System.nanoTime();
+        System.out.println((endTime - startTime) / 1000000 + "ms");
         return outputResult;
     }
 }
