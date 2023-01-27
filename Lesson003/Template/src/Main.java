845abed1a38573c77c80d82063f57c8d9d2c2eac
ShadowGreg <shadow-greg@yandex.ru>
2023-01-27 19:38:38 +0300

    Прмиенили ООП

--- Lesson003/Template/src/Main.java
+++ Lesson003/Template/src/Main.java
@@ -1,16 +1,50 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Рисуем флаг России:");
-        System.out.println("Флагшток нарисован");
-        System.out.println("Полоса белого цвета нарисована");
-        System.out.println("Полоса синего цвета нарисована");
-        System.out.println("Полоса красного цвета нарисована");
+        RussianFlag russianFlag = new RussianFlag();
+        russianFlag.drawFlag();
 
         System.out.println("\nРисуем флаг Нидерландов:");
+        NetherlandsFlag netherlandsFlag = new NetherlandsFlag();
+        netherlandsFlag.drawFlag();
+    }
+}
+class Colors {
+    static void paintWhiteColor(){
+        System.out.println(ConsoleColors.WHITE_BACKGROUND_BRIGHT + "ПОЛОСА\tБЕЛОГО\t\tЦВЕТА" + ConsoleColors.RESET);
+    }
+    static void paintBlueColor(){
+        System.out.println(ConsoleColors.BLUE_BACKGROUND + "ПОЛОСА\tСИНЕГО\t\tЦВЕТА" + ConsoleColors.RESET);
+    }
+    static void paintRedColor(){
+        System.out.println(ConsoleColors.RED_BACKGROUND + "ПОЛОСА\tКРАСНОГО\tЦВЕТА" + ConsoleColors.RESET);
+    }
+}
+
+class RussianFlag {
+
+    void drawFlag() {
+        drawFlagpole();
+        Colors.paintWhiteColor();
+        Colors.paintBlueColor();
+        Colors.paintRedColor();
+    }
+
+    private void drawFlagpole() {
         System.out.println("Флагшток нарисован");
-        System.out.println("Полоса красного цвета нарисована");
-        System.out.println("Полоса белого цвета нарисована");
-        System.out.println("Полоса синего цвета нарисована");
+    }
+}
+
+class NetherlandsFlag {
+
+    void drawFlag() {
+        drawFlagpole();
+        Colors.paintRedColor();
+        Colors.paintWhiteColor();
+        Colors.paintBlueColor();
+    }
+
+    private void drawFlagpole() {
+        System.out.println("Флагшток нарисован");
     }
 }
