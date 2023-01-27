bbfaf6ad2db160cc53c1b15ae825e315bfe9fc5d
ShadowGreg <shadow-greg@yandex.ru>
2023-01-27 19:09:44 +0300

    Написали на пролом

---
+++ Lesson003/Template/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Рисуем флаг России:");
+        System.out.println("Флагшток нарисован");
+        System.out.println("Полоса белого цвета нарисована");
+        System.out.println("Полоса синего цвета нарисована");
+        System.out.println("Полоса красного цвета нарисована");
+
+        System.out.println("\nРисуем флаг Нидерландов:");
+        System.out.println("Флагшток нарисован");
+        System.out.println("Полоса красного цвета нарисована");
+        System.out.println("Полоса белого цвета нарисована");
+        System.out.println("Полоса синего цвета нарисована");
+    }
+}
