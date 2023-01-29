0aca52254e681b7a534a2fa9ba9f57f3d7eb7372
ShadowGreg <shadow-greg@yandex.ru>
2023-01-29 09:58:45 +0300

    all done

---
+++ Lesson004/Seminar/Tasks/src/Task000.java
@@ -0,0 +1,5 @@
+public class Task000 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Lesson004/Seminar/Tasks/src/Task001.java
@@ -0,0 +1,5 @@
+public class Task001 {
+    public static void main(String[] args) {
+
+    }
+}
