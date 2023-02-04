516bff40d9778ed4451c2ef412676132510fdf7a
ShadowGreg <shadow-greg@yandex.ru>
2023-02-04 17:02:47 +0300

    Готова домашка к 5 семинару

--- Lesson005/Homework/Homework.java
+++ Lesson005/Homework/Homework.java
@@ -1,23 +1,6 @@
-import java.util.*;
-
 public class Homework {
-    static void main(String[] args) {
-//        String text = "Иван Иванов 7898798754987 794549879879  987987324654";
-        Map<String, List<String>> phoneBook = new HashMap<>();
-        getDuplicateNames(phoneBook);
-        printSortMap(phoneBook);
-
-        String myString = "ывапывда 1234";
-        int foo = Integer.parseInt(myString);
-
-
+    public static void main(String[] args) {
+        PhoneBookController phoneBookController = new PhoneBookController();
+        phoneBookController.printNamesList();
     }
-
-    private static void printSortMap(Map<String, List<String>> phoneBook) {
-    }
-
-    private static void getDuplicateNames(Map<String, List<String>> phoneBook) {
-    }
-
-
 }
---
+++ Lesson005/Homework/PhoneBookEntry.java
@@ -0,0 +1,33 @@
+public class PhoneBookEntry {
+    private String name = "";
+    private String surname = "";
+    private String nameAndSurname = "";
+    private String phoneNumber = "";
+
+    public PhoneBookEntry(String inputText) {
+        int name = 0;
+        int surname = 1;
+        int phoneNumber = 2;
+        String[] tempText = inputText.split(" ");
+        this.name = tempText[name];
+        this.surname = tempText[surname];
+        this.phoneNumber = tempText[phoneNumber];
+        this.nameAndSurname = (new StringBuilder(tempText[name] + " " + tempText[surname])).toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhoneNumbers() {
+        return phoneNumber;
+    }
+
+    public String getNameAndSurname() {
+        return nameAndSurname;
+    }
+}
